7b0558ac6cd856042dac0708d8e1ace81d6452ae
sami <samialabdalah@yahoo.com>
2023-12-29 19:04:28 +0100

    work with CI/CD

---
+++ WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Products";
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            ViewData["Title"] = "Search for products";
+            return View();
+        }
+    }
+}
